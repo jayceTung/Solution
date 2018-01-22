49cb8b3094b7815aea2cd2f719d7fbe1e4b64cf5
Super <minchao.dong@melot.cn>
2018-01-22 18:01:15 +0800

    feature: pandas and numpy

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+import numpy as np
+import pandas as pd
 
 
 class Director(object):
     result = []
+    METHOD = 'method hello'
 
     def __init__(self, name):
         self.name = name
@@ -14,8 +17,38 @@ class Director(object):
     def get_name(self):
         return self.name
 
+    def quicksort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        pivot = arr[int(len(arr) / 2)]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x < pivot]
+        right = [x for x in arr if x > pivot]
+        return middle
+
+    @staticmethod
+    def test3():
+        print(Director.METHOD)
+        print('test3')
+
+    @classmethod
+    def test2(cls):
+        print(cls)
+        print('test2')
+
+
 if __name__ == '__main__':
     director = Director('天天')
     print(director.get_name())
+    test = [3, 6, 8, 10, 1, 2, 1]
+    print(director.quicksort(test))
+    Director.test3()
+    Director.test2()
+    a = np.arange(30)
+    a.shape = 2, -1, 3
+    print(a.shape)
+    print(a)
+    s = pd.Series(np.array([1, 1, 2, 3, 5, 8]))
+    print(s)
 
 
